efcac59f47bf57f79a50c3159840c9c0c5d341be
Armaan-zsh <shaikhmushrraff68@gmail.com>
2026-02-23 20:18:00 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,6 @@ export default function HomePage() {
           )}
         </div>
       </main>
-      <Footer sourceCount={stats?.sourceCount} postCount={stats?.postCount} />
 
       {readingPost && (
         <ReaderModal
